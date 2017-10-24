920ee7b145910b9a907dd2a1a5d79040b8dc335e
armenvod <32927742+armenvod@users.noreply.github.com>
2017-10-24 10:38:16 +0100

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,13 +1,16 @@
+
+
+
 from fonAPI import FonApi
 
 fon = FonApi('401bfde5401d0bdce2edddba6bcaa1d4bc8e4d17091b92cd')
 
 #brand = 'Samsung '
-phone1 = 'Apple iPhone 6s Plus, iOS, 5.5", 4G LTE, SIM Free, 128GB,'
+phone1 = 'Samsung Galaxy J3 Smartphone, Android, 5", 4G LTE, SIM Free, 8GB,'
 
 
 
-def brand_search(phone):
+'''def brand_search(phone):
     result = []
     phone_split = phone.split()
     string_length = len(phone_split)
@@ -36,28 +39,30 @@ def brand_search(phone):
                 repeated.add(i)
             else:
                 seen.add(i)
-    print(repeated)
-
+    print(repeated)'''
 
 
 def phone_search(phone):
-    phone_clean = phone.replace(',','')
-    result = []
+    '''function cycles through the website string and searches using fonoAPI.
+     It shaves off a word from the string every time until the first phone match is found.'''
+    phone_clean = phone.replace(',','') # removes commas from string
+    result = ''
     l = 0
-    while l< len(phone_clean)-2:
+    while l< len(phone_clean):
         empty = ''
-        phones = fon.getdevice(phone_clean.rsplit(' ',l)[0])
-        try:
+        phones = fon.getdevice(phone_clean.rsplit(' ',l)[0]) # deletes the "l"th word
+        try: # fonoAPI name match command
             for phone in phones:
                 empty = phone['DeviceName']
 
         except:
             empty = phones
-        #if empty[-1]
-        l = l + 1
-        result.append(empty)
-    result[:] = [x for x in result if x != u'No Matching Results Found.']
-    print result
+        if empty != u'No Matching Results Found.':
+            result = empty # result is equal to the first result found by fonoAPI
+            break
+        else:
+            l = l + 1
+    print(result)
 
 phone_search(phone1)
 
